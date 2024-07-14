68a68d4f0496065ea875edba29f21fb6d3be9e42
Sargentogato <marlonjvelasquez@gmail.com>
2024-07-14 23:41:58 +0200

    add docker for backend and frontend add docker-compose

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+// backend/index.js
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Importar rutas
+// const apiRoutes = require("./routes/api");
+// app.use("/api", apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Backend running at http://localhost:${port}`);
+});
